b663628dd22a2996fff50a7b1f6f93c71f2490d1
ayanadamtew <139906412+ayanadamtew@users.noreply.github.com>
2024-11-15 11:41:23 +0300

    Final updates and deployed to Render

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const app = Express();
 
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Welcome to the Simple Web Server!</h1>
+    <p>Use the following routes to get information:</p>
+    <ul>
+      <li><a href="/name">/name</a>:My full name</li>
+      <li><a href="/hobby">/hobby</a>: My favorite hobby</li>
+      <li><a href="/dream">/dream</a>: A motivational message about My dream or goal</li>
+    </ul>
+  `);
+});
 
 app.get('/name', (req, res) => {
   res.status(200).send("Ayana Damtew")
