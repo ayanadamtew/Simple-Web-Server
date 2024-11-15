2e8909745c99e5270ab7d5148e22026a913f4143
ayanadamtew <139906412+ayanadamtew@users.noreply.github.com>
2024-11-15 10:30:52 +0300

    Initial project setup with basic server file and package.json

---
+++ server.js
@@ -0,0 +1,23 @@
+const Express = require('express');
+
+const app = Express();
+
+const port = process.env.PORT || 3000;
+
+
+app.get('/name', (req, res) => {
+  res.status(200).send("Ayana Damtew")
+})
+
+app.get('/hobby', (req,res) => {
+  res.status(200).json({Hobby: 'Playing video games'})
+})
+
+app.get('/dream', (req, res) => {
+  res.send('my dream is being successfull sofftware engineer that changMy dream is to become a successful software engineer and make a positive impact in the world through technology.')
+})
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`)
+
+})
